8324b914c3e1beae7899d0ab50f979ff6b85814b
LBN <fatahillaqsa@gmail.com>
2024-09-10 20:05:26 +0700

    feat: add pgsql typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'be-suwit-postgres',
+      port: 6190,
+      username: 'kel2',
+      password: 'kel2',
+      database: 'suwit',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
