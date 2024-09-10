0054c32129434fb37d0e0e6fdec49a6d149728c1
harisyf <yafie345@gmail.com>
2024-09-10 20:52:40 +0700

    feat: update user.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Version } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,27 +8,28 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Version('1')
+  async create(): Promise<any> {
+    return "create"
+
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @Get('self')
+  @Version('1')
+  async getSelf(): Promise<any> {
+    return "get"
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Patch('self')
+  @Version('1')
+  async updateSelf(): Promise<any> {
+    return "update"
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Delete('self')
+  @Version('1')
+  async deleteSelf(): Promise<any> {
+    return "delete"
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
 }
