ec3efadd00ddc1a3f7132a1132c72a4f672fa9c5
harisyf <yafie345@gmail.com>
2024-09-10 21:03:07 +0700

    feat: create user entity table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique:true })
+    username: string;
+
+    @Column()
+    password: string;
+}
